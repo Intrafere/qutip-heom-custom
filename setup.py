74635cc3e52f530af7351fe589b26e10e282a360
Intrafere <phwhite414@gmail.com>
2025-04-23 20:05:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
  
+from setuptools import setup, find_packages
+
+setup(
+    name='qutip_heom',
+    version='0.1.0',
+    description='Custom HEOM solver for non-Markovian quantum systems using QuTiP.',
+    author='Intrafere',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.20',
+        'scipy>=1.5',
+        'qutip>=4.6'
+    ],
+    python_requires='>=3.7',
+)
