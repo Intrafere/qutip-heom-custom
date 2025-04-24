ea3efe391ed2755542294c1a9c2f48a2c227dc96
Intrafere <phwhite414@gmail.com>
2025-04-23 20:06:21 -0500

    Update bosonic_bath.py

--- qutip_heom/heom/bosonic_bath.py
+++ qutip_heom/heom/bosonic_bath.py
@@ -1,1 +1,8 @@
- 
+class BosonicBath:
+    def __init__(self, gamma, lam, beta):
+        self.gamma = gamma    # decay rate
+        self.lam = lam        # reorganization energy
+        self.beta = beta      # 1/(k_B * T)
+
+    def spectral_density(self, omega):
+        return 2 * self.lam * self.gamma * omega / (omega**2 + self.gamma**2)
